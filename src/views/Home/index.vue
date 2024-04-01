a160720cd24254199a9ad9878ba160712d799993
Welen1911 <welenalmeida2210@gmail.com>
2024-04-01 15:13:54 -0300

    Feito tela inicial parcialmente

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,31 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+
+import CustomHeader from './CustomHeader.vue';
+import Contact from './Contact.vue';
+
+function handleLogin() {
+
+}
+
+function handleCreateAccount() {
+
+}
+
+const router = useRouter();
+
+onMounted(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+        router.push({ name: "FeedBacks" });
+    }
+});
+</script>
 <template>
-    <div class="font-black text-brand-main">
-        Home!
+    <CustomHeader @createAccount="handleCreateAccount" @login="handleLogin" />
+    <Contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+        <p class="font-medium text-center text-gray-800">FeedBacker© 2024</p>
     </div>
 </template>
