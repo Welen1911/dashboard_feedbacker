584f7e55e9e61cdad2ac90b28f2ba41ab7ef55b9
Welen1911 <welenalmeida2210@gmail.com>
2024-04-17 19:40:32 -0300

    Feito: alteracao para usar o back na branch single

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -1,10 +1,19 @@
 <script setup>
+import { reactive } from 'vue';
+
 import HeaderLogger from '../../components/HeaderLogger/index.vue';
+import ContentLoader from '../../components/ContentLoader/index.vue';
 import Icon from '../../components/Icon/index.vue';
 import useStore from '../../hooks/useStore';
 import palette from '../../../palette';
 
 const store = useStore('User');
+const global = useStore('Global');
+
+
+const state = reactive({
+    isLoading: false,
+});
 
 const brandColors = palette.brand;
 </script>
@@ -26,9 +35,15 @@ const brandColors = palette.brand;
             <p class="mt-10 text-lg text-gray-800 font-regular">
                 Este aqui é a sua chave de api
             </p>
-            <div v-for="apikey in store.currentUser.apikeys" :key="apikey.id"
-                class="flex py-3 pl-5 mt-2 rounded items-center bg-brand-gray w-2/3">
-                <span>{{ apikey.id }}</span>
+            <ContentLoader
+            v-if="global.isLoading || state.isLoading"
+            class="rounded"
+            widt="600px"
+            height="50px"
+            />
+            <div v-else
+                class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-2/3">
+                <span>{{ store.currentUser.apikey.id }}</span>
                 <div class="flex ml-80 mr-1">
                     <Icon name="copy" :color="brandColors.graydark" size="24" class="cursor-pointer"></Icon>
                     <Icon name="loading" :color="brandColors.graydark" size="24" class="cursor-pointer ml-3"></Icon>
@@ -37,8 +52,15 @@ const brandColors = palette.brand;
             <p class="mt-5 text-lg text-gray-800 font-regular">
                 Coloque o script abaixo no seu site para começar a receber feedbacks
             </p>
-            <div v-for="apikey in store.currentUser.apikeys" :key="apikey.id" class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3 overflow-x-scroll">
-                <pre>&lt;script src="https://Welen1911-feedbacker-widget.netlify.app?apikey={{ apikey.id }}"&gt;&lt;/script&gt;</pre>
+            <ContentLoader
+            v-if="global.isLoading || state.isLoading"
+            class="rounded"
+            widt="600px"
+            height="50px"
+            />
+            <div v-else 
+            class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll">
+                <pre>&lt;script src="https://Welen1911-feedbacker-widget.netlify.app?apikey={{ store.currentUser.apikey.id }}"&gt;&lt;/script&gt;</pre>
             </div>
         </div>
     </div>
